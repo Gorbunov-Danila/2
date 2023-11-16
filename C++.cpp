42732f63913322c3589fc8d0e4435a8459a48a0b
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2023-11-16 14:27:34 +0300

    1

---
+++ C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i <= 103450; i++) {
+        sum += i;
+        std::cout << "—умма чисел от 1 до " << i << ": " << sum << std::endl;
+    }
+    return 0;
+}
