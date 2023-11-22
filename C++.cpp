b3409ef83b408cb78ecfe7b230f92c52bad7a489
Gorbunov-Danila <gorbunovdanil345@gmail.com>
2023-11-22 18:25:02 +0300

    1

--- C++.cpp
+++ C++.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     int sum = 0;
-    for (int i = 1; i <= 10000; i++) {
+    for (int i = 1; i <= 100000; i++) {
         sum += i;
         std::cout << "—умма чисел от 1 до " << i << ": " << sum << std::endl;
     }
